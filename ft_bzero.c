3423c8f10429da33f79a9ab02804025a5c7360f9
Ikram Kharbouch <ikrkharb@e1r8p11.1337.ma>
2019-03-26 17:36:09 +0100

    add_bzero

--- ft_bzero.c
+++ ft_bzero.c
@@ -1,36 +1,33 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_memset.c                                        :+:      :+:    :+:   */
+/*   ft_bzero.c                                         :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: ikrkharb <ikrkharb@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2019/03/26 16:46:10 by ikrkharb          #+#    #+#             */
-/*   Updated: 2019/03/26 16:50:21 by ikrkharb         ###   ########.fr       */
+/*   Created: 2019/03/26 16:58:41 by ikrkharb          #+#    #+#             */
+/*   Updated: 2019/03/26 17:34:12 by ikrkharb         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdio.h>
-#include <string.h>
 
-void	ft_memset(void *str, char c, size_t n)
+void	ft_bzero(void *s, int nbyte)
 {
-	size_t i;
+	char *str = (char *)s; //the cast
+	int i;
 
 	i = 0;
-	char *s = (char *)str;
-	while (i < n)
+	while (i < nbyte)
 	{
-		s[i] = c;
+		str[i] = 0;
 		i++;
-
 	}
 }
-
 int		main()
 {
-	char str[] = "hello";
-	ft_memset(str,'*', 2);
+	char str[] = "aello";
+	ft_bzero(str,0);
 	printf("%s",str);
 	return (0);
 }
